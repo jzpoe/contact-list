9fb8a282925983ceea3fb965817163b705a21092
jzpoe <pmguez1287@gmail.com>
2023-05-04 15:16:23 -0500

    lista de contactos

---
+++ lista.js
@@ -0,0 +1,31 @@
+// Definición del array de contactos
+// const Agenda = [
+//     {name:"jose rodriguez", correo:"pmguez1287@gmail.com", telefono:"3057767694"},
+//     {name:"sandra daza", correo:"pmguez1287@gmail.com", telefono:"3057767694"}
+//   ];
+  
+//   // Función para agregar un nuevo contacto
+//   function addagenda(name, correo, telefono) {
+//     const newContact = { name, correo, telefono };
+//     const newContact1 = { name, correo, telefono };
+
+//     Agenda.push(newContact);
+//     Agenda.push(newContact1);
+//     return newContact , newContact1 ;
+//   }
+  
+//   // Agregar un nuevo contacto
+//   const newContact = addagenda("luna maria", "ppslsps@gmail.com", "3057765533");
+//   const newContact1 = addagenda("yuli", "ppslsps@gmail.com", "3057765533");
+  
+// function eliminarContacto(Agenda, indice){
+//  Agenda.splice(Agenda, 1)
+//  return Agenda;
+
+// }
+
+//   // Mostrar el nuevo contacto por consola
+//   console.log(Agenda, newContact , newContact1);
+//   console.log(eliminarContacto);
+
+
