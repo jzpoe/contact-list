4667906f0ef5b8d9c0d0b3a5a0fa984f576a8c6f
jzpoe <pmguez1287@gmail.com>
2023-05-06 07:35:34 -0500

    se crea nueva rama y se crean nuevas variables

--- lista.js
+++ lista.js
@@ -1,31 +1,38 @@
-// Definición del array de contactos
-// const Agenda = [
-//     {name:"jose rodriguez", correo:"pmguez1287@gmail.com", telefono:"3057767694"},
-//     {name:"sandra daza", correo:"pmguez1287@gmail.com", telefono:"3057767694"}
-//   ];
+//Definición del array de contactos
+
+const Agenda = [
+  {  name: "jose ", apellido: "rodriguez", telefono: "3057767694", ubicacion: "cali-valle", direccion: "cr cl" },
+  { name: "sandra", apellido: "daza", telefono: "3057767694", ubicacion: "cali-valle", direccion: "cl cr" }
+];
+
+let myid = 1;
+
+
+// Función para agregar un nuevo contacto
+function addagenda(name, apellido, telefono, ubicacion, direccion) {
+  const newContact = {id: myid++, name, apellido, telefono, ubicacion, direccion };
+  const newContact1 = {id:myid++, myid: name, apellido, telefono, ubicacion, direccion };
   
-//   // Función para agregar un nuevo contacto
-//   function addagenda(name, correo, telefono) {
-//     const newContact = { name, correo, telefono };
-//     const newContact1 = { name, correo, telefono };
 
-//     Agenda.push(newContact);
-//     Agenda.push(newContact1);
-//     return newContact , newContact1 ;
-//   }
+  Agenda.push(newContact);
+  Agenda.push(newContact1);
+  return newContact, newContact1;
   
-//   // Agregar un nuevo contacto
-//   const newContact = addagenda("luna maria", "ppslsps@gmail.com", "3057765533");
-//   const newContact1 = addagenda("yuli", "ppslsps@gmail.com", "3057765533");
-  
-// function eliminarContacto(Agenda, indice){
-//  Agenda.splice(Agenda, 1)
-//  return Agenda;
+}
 
-// }
+    
+// Agregar un nuevo contacto
+const newContact = addagenda("luna", "rodriguez", "333444333", "cali", "cl cr");
+const newContact1 = addagenda("yuli", "rodriguez", "22233", "cali", "cl cr");
 
-//   // Mostrar el nuevo contacto por consola
-//   console.log(Agenda, newContact , newContact1);
-//   console.log(eliminarContacto);
+function eliminarContacto(Agenda, indice) {
+  Agenda.splice(0, 1)
 
+  return Agenda;
 
+}
+
+// Mostrar el nuevo contacto por consola
+console.log(Agenda, newContact, newContact1);
+
+console.log(eliminarContacto);
